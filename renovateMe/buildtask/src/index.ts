4bd674bec357f7df08612c1703e5026780142dc3
Jean-Yves <jycouet@gmail.com>
2019-09-03 23:57:06 +0200

    various fix! (before clean git history)

--- renovateMe/buildtask/src/index.ts
+++ renovateMe/buildtask/src/index.ts
@@ -1,4 +1,5 @@
 import * as taskLib from "azure-pipelines-task-lib/task";
+import { IExecSyncResult } from "azure-pipelines-task-lib/toolrunner";
 
 interface ExecutionOptions {
   tool: string;
@@ -14,7 +15,7 @@ async function run(): Promise<void> {
   }
 
   // vsts Task Token
-  const token: string | undefined = taskLib.getEndpointAuthorizationParameter('SYSTEMVSSCONNECTION', 'ACCESSTOKEN', false);
+  const token: string | undefined = process.env["RENOVATE_TOKEN"] || taskLib.getEndpointAuthorizationParameter('SYSTEMVSSCONNECTION', 'ACCESSTOKEN', false);
   if (!token || token === '') {
     throw new Error(`You need to 'Allow scripts to access OAuth token' in the 'options' tab of your build system.`);
   }
@@ -39,30 +40,37 @@ async function run(): Promise<void> {
 
   // is yarn capable
   let isYarnCapable = false;
+  let yarnGlobalPath: string = '';
   try {
     taskLib.which('yarn', true);
     taskLib.debug('Yeahhh, yarn is installed!');
     isYarnCapable = true;
+    yarnGlobalPath = (await taskLib.execSync('yarn', 'global bin')).stdout.trim();
   } catch (error) {
     taskLib.warning(`yarn not found... don't worry we will use npm... but it will be slower...`);
   }
 
   // prepare to install renovate
-  const renovateInstallOptions: ExecutionOptions = isYarnCapable
+  const renovateInstallCmd: ExecutionOptions = isYarnCapable
     ? { tool: 'yarn', arguments: `global add renovate@${renovateOptionsVersion}` }
     : { tool: 'npm', arguments: `install -g renovate@${renovateOptionsVersion}` };
 
   // install renovate
   taskLib.debug(`Install renovate`);
-  await exec(renovateInstallOptions);
+  await exec(renovateInstallCmd);
 
   // prepare run renovate
-  const renovateArgs: string = `${project}/${repo} --platform azure --endpoint ${endpointAndCollection} --token ${token} ${renovateOptionsArgs}`;
+  const renovateArgs: string = `\"${project}\"/\"${repo}\" --platform azure --endpoint ${endpointAndCollection} --token ${token} ${renovateOptionsArgs}`;
   taskLib.debug(`renovateArgs to run: ${renovateArgs}`);
+  const runRenovateCmd: ExecutionOptions = isYarnCapable
+    ? { tool: yarnGlobalPath + '\\renovate', arguments: `${renovateArgs}` }
+    : { tool: 'renovate', arguments: `${renovateArgs}` };
 
   // run renovate
   taskLib.debug(`Run renovate`);
-  await exec({ tool: 'renovate', arguments: `${renovateArgs}` });
+  //await exec({ tool: 'npx', arguments: `renovate@${renovateOptionsVersion} ${renovateArgs}` });
+  // await exec({ tool: 'renovate', arguments: `${renovateArgs}` });
+  await exec(runRenovateCmd);
 
   // the end!
   taskLib.debug(`Renovate done`);
